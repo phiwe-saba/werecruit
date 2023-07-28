8ad26162e126c51b9c04b8b0a25509692ae91a1f
phiwe saba <sabasbulele81@gmail.com>
2023-07-28 22:31:11 +0200

    modified the admin dashboard...

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,18 +2,9 @@
 @section('content')
 <div class="container">
     <h1 style="text-align: center">Welcome</h1>
-    <div class="card" style="display: flex;">
     
-        <div class="card-body">
-            <h1>Welcome to the Admin Panel, use the sidebar to navigate between the different options.</h1>
-        </div>
-        <div class="card-body">
-            <h1>Welcome to the Admin Panel, use the sidebar to navigate between the different options.</h1>
-        </div>
-        <div class="card-body">
-            <h1>Welcome to the Admin Panel, use the sidebar to navigate between the different options.</h1>
-        </div>
+   
+</div>
 
-    </div>
 </div>
 @endsection
--- resources/views/vacancies/show.blade.php
+++ resources/views/vacancies/show.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.nav')
 @section('content')
 <div class="container">
-    <h3>Job Title: {{ $vacancy->job_title }}</h3>
-    <p>Job Field: {{ $vacancy->job_field }}</p>
-    <p>Location: {{ $vacancy->location }}</p>
-    <p>Job Type: {{ $vacancy->job_type }}</p>
-    <p>Description: {{ $vacancy->description }}</p>
-    <button class="btn btn-primary"><a href="{{ route('application.create', $vacancy) }}">Apply</a></button>
+    <div class="details py-5">
+        <h3>{{ $vacancy->job_title }}</h3>
+        <p>{{ $vacancy->location }} | {{ $vacancy->job_type }}</p>
+        <p>{{ $vacancy->job_field }}</p>
+        <p>Description: {{ $vacancy->description }}</p>
+        <button class="btn btn-primary"><a href="{{ route('application.create', $vacancy) }}">Apply</a></button>
+    </div>
 </div>
 @endsection
